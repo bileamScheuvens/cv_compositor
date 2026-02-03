be5ca1e7b95f7d8d40303d03fe5db0b0eec10f0c
bileamScheuvens <benedictbileam@gmx.de>
2026-02-03 19:41:10 +0100

    flakify

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A flake for a python environment.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # Adjust if needed
+      pkgs = import nixpkgs {
+        system = system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          python3
+          (python3.withPackages (
+            ps: with ps; [
+              # example of packaged an unpackaged dep
+              pyyaml
+              streamlit
+            ]
+          ))
+        ];
+
+        shellHook = ''
+          unset SOURCE_DATE_EPOCH
+
+          # Add necessary paths for dynamic linking
+          export LD_LIBRARY_PATH=${
+            pkgs.lib.makeLibraryPath ([
+              "/run/opengl-driver" 
+            ] ++ buildInputs)
+          }:$LD_LIBRARY_PATH
+
+        '';
+      };
+    };
+}
